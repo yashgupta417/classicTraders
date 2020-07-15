a33fc025a28b35e5c192c273fbae3ea3bd51d991
Yash <yashgupta417@gmail.com>
2020-07-15 23:38:28 +0530

    configuring for heroku

--- classicTraders/settings.py
+++ classicTraders/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,7 +132,7 @@ STATIC_DIR=os.path.join(BASE_DIR,"staticfiles")
 MEDIA_DIR=os.path.join(BASE_DIR,"media")
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(PROJECT_ROOT, 'static'),]
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
