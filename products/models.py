0f5d01a9f18edf8580e9d5f08eb4be240886cd4d
Yash <yashgupta417@gmail.com>
2020-07-20 19:55:38 +0530

    updated Product Model

--- products/models.py
+++ products/models.py
@@ -7,22 +7,22 @@ class Product(models.Model):
     name=models.CharField(max_length=255,blank=False,null=False)
     image=models.ImageField(upload_to='images/products/',null=True,blank=True)
 
-    purchase_price=models.DecimalField(max_digits=10,decimal_places=2)
-    purchase_unit=models.CharField(max_length=10)
+    purchase_price=models.DecimalField(max_digits=10,decimal_places=2,null=False,blank=False)
+    purchase_unit=models.CharField(max_length=10,null=False,blank=False)
 
-    min_retail_price=models.DecimalField(max_digits=10,decimal_places=2)
-    max_retail_price=models.DecimalField(max_digits=10,decimal_places=2)
-    retail_unit=models.CharField(max_length=10)
+    min_retail_price=models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
+    max_retail_price=models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
+    retail_unit=models.CharField(max_length=10,null=True,blank=True)
 
-    min_wholesale_price=models.DecimalField(max_digits=10,decimal_places=2)
-    max_wholesale_price=models.DecimalField(max_digits=10,decimal_places=2)
+    min_wholesale_price=models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
+    max_wholesale_price=models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
     wholesale_unit=models.CharField(max_length=10)
 
     added_by=models.ForeignKey(get_user_model(),related_name='products_added',on_delete=models.CASCADE)
-    added_on=models.IntegerField()
+    added_on=models.IntegerField(null=True,blank=True)
 
     last_updated_by=models.ForeignKey(get_user_model(),related_name='products_last_updated',on_delete=models.CASCADE)
-    last_updated_on=models.IntegerField()
+    last_updated_on=models.IntegerField(null=True,blank=True)
 
     def __str__(self):
         return self.name
